107711c0ed12bca0c14d800f6b07c757c5bfb88c
Chandrasekhar, Muthyala <cmuth001@odu.edu>
2020-08-07 04:18:32 -0400

    added carousal to the portfolio

---
+++ cmuthyala/apps/portfolio/src/app/about/carousel-basic.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngbd-carousel-basic',
+  templateUrl: './carousel-basic.html',
+  styleUrls: ['./carousel-basic.scss']
+})
+export class NgbdCarouselBasic {
+    showNavigationArrows = false;
+    showNavigationIndicators = true;
+  images = [
+        `/assets/carousal-image1.jpeg`,
+        `/assets/carousal-image2.jpeg`,
+        `/assets/carousal-image3.jpeg`    
+];
+}
