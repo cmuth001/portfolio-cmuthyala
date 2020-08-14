9c1499c5fc4ee140b1ac527eafd36be91ed37360
Chandrasekhar, Muthyala <cmuth001@odu.edu>
2020-08-14 16:32:20 -0400

    added education

--- cmuthyala/apps/portfolio/src/app/testimonies/testimonies.component.ts
+++ cmuthyala/apps/portfolio/src/app/testimonies/testimonies.component.ts
@@ -9,10 +9,14 @@ export class TestimoniesComponent implements OnInit {
 
   constructor() { }
   isCollapsed = false;
+  testimonyToggleText = "VIEW MORE";
+  testimonyToggleIcon = "expand_more";
   ngOnInit(): void {
   }
 
   toggleViewMore(){
     this.isCollapsed  = !this.isCollapsed;
+    this.testimonyToggleText = this.isCollapsed ?  "VIEW LESS" : "VIEW MORE" ;
+    this.testimonyToggleIcon = this.isCollapsed ? "expand_less" : "expand_more";
   }
 }
