eae70c72e0231b79c28e78734aa6150570d165f4
Chandrasekhar, Muthyala <cmuth001@odu.edu>
2020-08-24 21:23:05 -0400

    added 'see more' for experience

--- cmuthyala/apps/portfolio/src/app/experience/experience.component.ts
+++ cmuthyala/apps/portfolio/src/app/experience/experience.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-
+  seeMoreHighmarkText = '...see more';
+  seeMoreHighmarkToggle = false;
+  seeMoreOduText1 = "...see more"
+  seeMoreOduToggle1 = false;
+  seeMoreOduText2 = "...see more"
+  seeMoreOduToggle2 = false;
+  seeMoreSynopsysText = "..see more"
+  seeMoreSynopsysToggle = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  highmarkSeeMoreToggle(){
+    this.seeMoreHighmarkToggle = !this.seeMoreHighmarkToggle;
+    this.seeMoreHighmarkText = this.seeMoreHighmarkToggle ? "see less" : "...see more"
+    // console.log(this.seeMoreHighmarkText, this.seeMoreHighmarkToggle);
+  }
+  oduSeeMoreToggle1(){
+    this.seeMoreOduToggle1 = !this.seeMoreOduToggle1;
+    this.seeMoreOduText1 = this.seeMoreOduToggle1 ? "see less" : "...see more";
+  }
+  oduSeeMoreToggle2(){
+    this.seeMoreOduToggle2 = !this.seeMoreOduToggle2;
+    this.seeMoreOduText2 = this.seeMoreOduToggle2 ? "see less" : "...see more";
+  }
+  synopsysSeeMoreToggle(){
+    this.seeMoreSynopsysToggle = !this.seeMoreSynopsysToggle;
+   this.seeMoreSynopsysText =  this.seeMoreSynopsysToggle ? "see less" : "...see more";
+  }
+
 
 }
